a14ad18bff66cfb9f06f3223d36bcb2fe67d9ec6
rhys <rbrooker105@gmail.com>
2016-09-25 20:34:27 +1000

    Accept shift variable via web form, build index.erb with any shift

--- server.rb
+++ server.rb
@@ -2,10 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 require_relative 'caesar_split'
 get '/' do
-	to_cypher = params["encrypt"]
-	response = CaesarCipher.encrypt(to_cypher, 3) unless to_cypher == nil
+	to_cypher,shift = params["encrypt"],params["shift"]
+	to_cypher = check_input(to_cypher)
+	response = CaesarCipher.encrypt(to_cypher, shift.to_i)
   erb :index, :locals => {:response => response}
 end
 
-
-
+def check_input(cypher)
+	if cypher == ""
+		cypher = "You should put your text in the field before clicking submit"
+	end
+	return cypher
+end
