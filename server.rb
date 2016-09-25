ab12793e0a39340ea643fa8e20d54beef5c33798
rhys <rbrooker105@gmail.com>
2016-09-25 20:47:29 +1000

    Fix bug for no form input

--- server.rb
+++ server.rb
@@ -3,14 +3,18 @@ require 'sinatra/reloader'
 require_relative 'caesar_split'
 get '/' do
 	to_cypher,shift = params["encrypt"],params["shift"]
-	to_cypher = check_input(to_cypher)
-	response = CaesarCipher.encrypt(to_cypher, shift.to_i)
+	to_cypher,shift = check_input(to_cypher,shift)
+	response = CaesarCipher.encrypt(to_cypher, shift)
   erb :index, :locals => {:response => response}
 end
 
-def check_input(cypher)
-	if cypher == ""
-		cypher = "You should put your text in the field before clicking submit"
+def check_input(cypher, shift)
+	if cypher == "" || cypher == nil
+		cypher = "Did you forget something?"
 	end
-	return cypher
+	if shift == "" || shift == nil
+		shift = 0
+	end
+	shift = shift.to_i
+	return cypher, shift
 end
